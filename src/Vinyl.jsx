08fd1cca8be597f54ceca552db56c75b03fbcbc5
ivan <ivann97n@gmail.com>
2026-05-02 17:27:06 +0300

    Rebuild eclipse flame effect

--- src/Vinyl.jsx
+++ src/Vinyl.jsx
@@ -3,7 +3,7 @@ import vinylImage from '../vinyl.png';
 export default function Vinyl() {
   return (
     <div className="vinyl-wrapper" aria-label="Vinyl record">
-      <span className="vinyl-halo" aria-hidden="true" />
+      <span className="solar-corona" aria-hidden="true" />
       <img
         className="vinyl"
         src={vinylImage}
